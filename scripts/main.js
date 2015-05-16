a3b97a3c77aeb4f8b6e0a1925acb5b5299796340
Shan1024 <gambit1024@gmail.com>
2015-05-16 23:07:46 +0530

    Continues play added

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,32 @@
 
 $(function(){
 
+  var width = 720;
+  var animationSpeed = 1000;
+  var pause = 3000;
+  var currentSlide = 1;
+
+  //cache DOM
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slides');
+  var $slides = $slideContainer.find('.slide');
+
   setInterval(function(){
-    $('#slider .slides').animate({'margin-left': '-=720px'},1000);
-  },3000);
-  
+
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+
+      currentSlide++;
+
+      if(currentSlide == $slides.length){
+
+        currentSlide = 1;
+        
+        $slideContainer.css('margin-left', 0);
+
+      }
+
+    });
+
+  }, pause);
+
 });
